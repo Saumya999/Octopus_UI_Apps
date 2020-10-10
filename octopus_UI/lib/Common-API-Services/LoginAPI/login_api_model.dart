3fbb8381c2c7448135d423de9bc4cb872474aa8f
Saumya999 <saumyadiptasarkar49@gmail.com>
2020-10-10 18:15:19 +0530

    Login Error Handling

--- octopus_UI/lib/Common-API-Services/LoginAPI/login_api_model.dart
+++ octopus_UI/lib/Common-API-Services/LoginAPI/login_api_model.dart
@@ -5,18 +5,25 @@ class User {
   final String firstName;
   final String lastName;
   final String email;
+  final String error;
 
 
-  User({this.userId, this.firstName, this.lastName, this.email});
+  User({this.userId, this.firstName, this.lastName, this.email, this.error});
 
   factory User.fromJson(Map<String, dynamic> json) {
 
-    return User(
-      userId: json['id'],
-      firstName: json['firstName'],
-      lastName: json['lastName'],
-      email: json['email'],
+    if (json['email'] != null) {
+      return User(
+        userId: json['id'],
+        firstName: json['firstName'],
+        lastName: json['lastName'],
+        email: json['email'],
       );
+    } else {
+      return User(
+        error: json['errorMessage']
+      );
+    }
   }
 }
 
